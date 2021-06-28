89c49171ffb62831206fef2517e2f2b87d42139f
alisa-pogarskaya <alisapogarskaya@gmail.com>
2021-06-28 15:08:11 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -35,3 +35,55 @@ function getRandomInclusiveDecimal(min, max, decimalPlace) {
 }
 
 getRandomInclusiveDecimal(-2.32732428348, 10.293737474, 3);
+
+
+// Генерация данных
+
+const DATABASE = {
+  author: {
+    avatar: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'], // строка — адрес изображения вида img/avatars/user{{xx}}.png, где {{xx}} — это число от 1 до 10. Перед однозначными числами ставится 0. Например, 01, 02...10. Адреса изображений не повторяются.
+  },
+  offer: {
+    title: ['Шарлотка', 'Леонардо', 'Монплезир', 'Биг-мак', 'Шарлотка', 'Шарлотка', 'Шарлотка', 'Шарлотка', 'Шарлотка', 'Шарлотка'],
+    address: [location.lat, location.lng],
+    price: 3, // Случайное целое положительное число
+    type: ['palace', 'flat', 'house', 'bungalow', 'hotel'],
+    rooms: 1, // Случайное целое положительное число.
+    guests: 1, // Случайное целое положительное число.
+    checkin: ['12:00', '13:00', '14:00'],
+    checkout: ['12:00', '13:00', '14:00'],
+    features: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'], // Значения не должны повторяться.
+    description: 'Чудесное место, где можно отведать фирменную шарлотку на Парашутной',
+    photos: ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg','https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg.'],
+  },
+  location: {
+    lat: getRandomIntInclusive(35.65000, 35.70000),
+    lng: getRandomIntInclusive(139.70000, 139.80000),
+  },
+};
+
+const SIMILAR_WIZARD_COUNT = 10;
+
+const getRandomArrayElement = (elements) => elements[getRandomIntInclusive(0, elements.length - 1)];
+
+const createAdvert = () => ({
+  // eslint-disable-next-line no-undef
+  avatar: `img/avatars/user${ getRandomArrayElement(DATABASE.author.avatar) }.png`,
+  title: getRandomArrayElement(DATABASE.offer.title),
+  address: DATABASE.location,
+  price: getRandomIntInclusive(0, 10000000),
+  type: getRandomArrayElement(DATABASE.offer.type),
+  rooms: getRandomIntInclusive(0, 500),
+  guests: getRandomIntInclusive(0, 10000000),
+  checkin: getRandomArrayElement(DATABASE.offer.checkin),
+  checkout: getRandomArrayElement(DATABASE.offer.checkout),
+  features: getRandomArrayElement(DATABASE.offer.features),
+  description: DATABASE.offer.description,
+  photos: getRandomArrayElement(DATABASE.offer.photos),
+  location: DATABASE.location.lat + DATABASE.location.lng,
+});
+
+const similarWizards = new Array(SIMILAR_WIZARD_COUNT).fill(null).map(() => createAdvert());
+
+// eslint-disable-next-line no-console
+console.log(similarWizards);
